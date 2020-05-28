dbc9cb92930b4c7fea91d9afda9b14d6e3e3c4d7
ismailkasan <ismailkasan@piton.com.tr>
2020-05-28 22:45:59 +0300

    ilk commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Confluent.Kafka;
+
+namespace KafkaConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //var consumergroup = Environment.GetEnvironmentVariable("CONSUMER_GROUP");
+            var topicName = Environment.GetEnvironmentVariable("TOPIC_NAME");
+            var brokerList = Environment.GetEnvironmentVariable("KAFKA_URL");
+
+            var config = new ConsumerConfig {GroupId = "KafkaConsumer", BootstrapServers = "localhost:9092"};
+
+            using (var consumer = new ConsumerBuilder<string, string>(config)
+                .SetRebalanceHandler( (obj, e) => {
+                    if (e.IsAssignment) Console.WriteLine($"Assigned partitions: [{string.Join(", ", e.Partitions)}]");
+                    else Console.WriteLine($"Revoked partitions: [{string.Join(", ", e.Partitions)}]");
+                }).Build())
+            {
+                consumer.Subscribe("ToDoList");
+                while (true)
+                {
+                    ConsumeResult<string, string> consumeResult = consumer.Consume();
+                    Console.WriteLine($"Received message at {consumeResult.TopicPartitionOffset}: {consumeResult.Value}");
+                    consumer.Commit();
+                }
+            }
+        }
+    }
+}
